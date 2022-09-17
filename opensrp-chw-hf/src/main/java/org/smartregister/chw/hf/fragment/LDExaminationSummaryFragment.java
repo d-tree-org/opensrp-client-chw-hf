637421dd3571c2a551fce8d8b6609893da52aaf2
maulidkategile <maulidkategile@gmail.com>
2022-09-17 17:15:37 +0300

    create the summary user interface

---
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/fragment/LDExaminationSummaryFragment.java
@@ -0,0 +1,25 @@
+package org.smartregister.chw.hf.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.smartregister.chw.hf.R;
+
+public class LDExaminationSummaryFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ld_examination_summary, container, false);
+    }
+}
