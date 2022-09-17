52b7458fa53e17119ee05815514f6748ac507418
maulidkategile <maulidkategile@gmail.com>
2022-09-17 20:33:46 +0300

    create summary's user interface

---
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/fragment/LDAmtslSummary.java
@@ -0,0 +1,25 @@
+package org.smartregister.chw.hf.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.smartregister.chw.hf.R;
+
+public class LDAmtslSummary extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ld_amtsl_summary, container, false);
+    }
+}
